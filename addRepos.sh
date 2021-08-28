8a63c67b6853698ad2d13e06fa06fae7c452f6e4
Nick Scardino <nick@scardino.dev>
2021-08-28 17:21:50 -0400

    changed clone command to https

--- addRepos.sh
+++ addRepos.sh
@@ -4,8 +4,8 @@ echo "Adding repos to '~/flat-remix'..."
 
 mkdir ~/flat-remix && cd ~/flat-remix
 
-git clone git@github.com:daniruiz/flat-remix.git
-git clone git@github.com:daniruiz/flat-remix-gnome.git
-git clone git@github.com:daniruiz/flat-remix-gtk.git
+git clone https://github.com:daniruiz/flat-remix.git
+git clone https://github.com:daniruiz/flat-remix-gnome.git
+git clone https://github.com:daniruiz/flat-remix-gtk.git
 
 echo "Repos added. Run update.sh to create symlinks"
